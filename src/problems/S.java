e1c0755a8bf1acaaa39501a6af81493395ce7fe0
Alexandre Mucci <email@alexandremucci.com.br>
2017-02-03 16:31:47 -0200

    FizzBuzz challenge

---
+++ src/problems/S.java
@@ -0,0 +1,9 @@
+package problems;
+
+/**
+ * 
+ * HackerRank Challenge
+ * Shorter class to solve FizzBuzz problem
+ *
+ */
+class S{public static void main(String[]a){for(int i=0;i++<100;System.out.println((i%3<1?"Fizz":"")+(i%5<1?"Buzz":i%3<1?"":i)));}}
